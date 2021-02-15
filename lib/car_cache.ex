1f8efa3578fb378cad13a4eb6f13cc29d86ea70d
Jeffery Utter <jeff@jeffutter.com>
2021-02-15 16:15:52 -0600

    Add initial property test

--- lib/car_cache.ex
+++ lib/car_cache.ex
@@ -33,7 +33,7 @@ defmodule CarCache do
     }
   end
 
-  @spec get(t(), any()) :: any()
+  @spec get(t(), any()) :: {any(), t()}
   def get(car, key) do
     case Clock.get(car.t1, key) do
       {nil, t1} ->
@@ -41,10 +41,14 @@ defmodule CarCache do
 
         car = %__MODULE__{car | t1: t1, t2: t2}
 
+        IO.inspect("CACHE HIT: T2")
+
         {value, car}
 
       {value, t1} ->
         car = %__MODULE__{car | t1: t1}
+
+        IO.inspect("CACHE HIT: T1")
 
         {value, car}
     end
@@ -85,7 +89,7 @@ defmodule CarCache do
 
       # cache directory hit
       LRU.member?(car.b1, key) ->
-        IO.inspect(key, label: "B1 Hit")
+        # IO.inspect(key, label: "B1 Hit")
 
         # Adapt:Increase the target size for the list T1 as:p=min{p+max{1,|B2|/|B1|},c}
         p = min(car.p + max(1, LRU.size(car.b2) / LRU.size(car.b1)), car.c)
@@ -99,7 +103,7 @@ defmodule CarCache do
       # cache directory hit
       # x must be in B2
       true ->
-        IO.inspect(key, label: "B2 Hit")
+        # IO.inspect(key, label: "B2 Hit")
 
         # Adapt:Decrease the target size for the list T1 as:p=max{p−max{1,|B1|/|B2|},0}
         p = max(car.p - max(1, LRU.size(car.b1) / LRU.size(car.b2)), 0)
@@ -115,33 +119,33 @@ defmodule CarCache do
   @spec replace(t()) :: t()
   def replace(car) do
     if Clock.size(car.t1) >= max(1, car.p) do
-      IO.inspect("Replace", label: "t1 > max(1, car.p)")
+      # IO.inspect("Replace", label: "t1 > max(1, car.p)")
 
       case Clock.pop(car.t1) do
         {key, value, 0, t1} ->
-          IO.inspect(0, label: "Ref Bit")
+          # IO.inspect(0, label: "Ref Bit")
           # Demote the head page in T1 and make it the MRU page in B1.
           b1 = LRU.insert(car.b1, key, value)
           %__MODULE__{car | b1: b1, t1: t1}
 
         {key, value, 1, t1} ->
-          IO.inspect(1, label: "Ref Bit")
+          # IO.inspect(1, label: "Ref Bit")
           # Set the page reference bit of head page in T1 to 0, and make it the tail page in T2.
           t2 = Clock.insert(car.t2, key, value)
           replace(%__MODULE__{car | t1: t1, t2: t2})
       end
     else
-      IO.inspect("Replace", label: "t1 <= max(1, car.p)")
+      # IO.inspect("Replace", label: "t1 <= max(1, car.p)")
 
       case Clock.pop(car.t2) do
         {key, value, 0, t2} ->
-          IO.inspect(0, label: "Ref Bit")
+          # IO.inspect(0, label: "Ref Bit")
           # Demote the head page in T2 and make it the MRU page in B2.
           LRU.insert(car.b2, key, value)
           %__MODULE__{car | t2: t2}
 
         {key, value, 1, t2} ->
-          IO.inspect(1, label: "Ref Bit")
+          # IO.inspect(1, label: "Ref Bit")
           # Set the page reference bit of head page in T2 to 0, and make it the tail page in T2.
           Clock.insert(car.t2, key, value)
           replace(%__MODULE__{car | t2: t2})
--- test/car_cache_test.exs
+++ test/car_cache_test.exs
@@ -2,7 +2,84 @@ defmodule CarCacheTest do
   use ExUnit.Case
   doctest CarCache
 
-  test "greets the world" do
-    assert CarCache.hello() == :world
+  use PropCheck
+  use PropCheck.StateM.ModelDSL
+
+  def key, do: oneof([atom(), utf8()])
+  def value, do: any()
+  def cache_name, do: atom()
+
+  @moduletag timeout: 120_000
+  property "stateful property", [:verbose, numtests: 500, max_size: 150] do
+    forall cmds <- commands(__MODULE__) do
+      trap_exit do
+        r = run_commands(__MODULE__, cmds)
+        {_history, _state, result} = r
+
+        (result == :ok)
+        |> when_fail(print_report(r, cmds))
+      end
+    end
+  end
+
+  def initial_state, do: %{cache: nil, inserted: %{}}
+
+  def command_gen(%{cache: nil}) do
+    {:create_cache, [cache_name()]}
+  end
+
+  def command_gen(%{cache: cache, inserted: inserted}) when inserted == %{} do
+    {:insert, [cache, key(), value()]}
+  end
+
+  def command_gen(%{cache: cache, inserted: inserted}) do
+    known_key =
+      inserted
+      |> Map.keys()
+      |> Enum.take_random(1)
+      |> List.first()
+
+    key = weighted_union([{1, known_key}, {4, key()}])
+
+    frequency([
+      {4, {:insert, [cache, key, value()]}},
+      {1, {:get, [cache, key]}}
+    ])
+  end
+
+  defcommand :create_cache do
+    def impl(name) do
+      CarCache.new(name)
+    end
+
+    def next(state, _args, cache) do
+      %{state | cache: cache}
+    end
+  end
+
+  defcommand :insert do
+    def impl(cache, key, value) do
+      CarCache.insert(cache, key, value)
+    end
+
+    def post(_state, [_cache, key, value], cache) do
+      {v, _} = CarCache.get(cache, key)
+      v == value
+    end
+
+    def next(state, [_cache, key, value], cache) do
+      %{cache: cache, inserted: Map.put(state.inserted, key, value)}
+    end
+  end
+
+  defcommand :get do
+    def impl(cache, key) do
+      {_, cache} = CarCache.get(cache, key)
+      cache
+    end
+
+    def next(state, [_cache, _key], cache) do
+      %{state | cache: cache}
+    end
   end
 end
