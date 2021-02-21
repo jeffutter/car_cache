ff12b127bc2620bdadc0b8ec29af77951fb04125
Jeffery Utter <jeff@jeffutter.com>
2021-02-20 21:05:15 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CarCache.MixProject do
   use Mix.Project
 
   @name "CarCache"
-  @version "0.1.0"
+  @version "0.1.1"
   @repo_url "https://github.com/jeffutter/car_cache"
 
   def project do
